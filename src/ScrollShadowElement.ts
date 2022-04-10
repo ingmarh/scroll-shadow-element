3359e212fe4f33d42fb7207b2cc453d7a22fd627
Ingmar Hergst <hergst@gmail.com>
2022-04-10 22:15:26 +0200

    Simplify file structure

--- src/ScrollShadowElement.ts
+++ src/ScrollShadowElement.ts
@@ -1,4 +1,32 @@
-import { Updater } from './Updater'
+const template = `
+  <style>
+    :host {
+      display: inline-block;
+      position: relative;
+    }
+
+    :host([hidden]) {
+      display: none;
+    }
+
+    s {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      pointer-events: none;
+      background:
+        var(--scroll-shadow-top, radial-gradient(farthest-side at 50% 0%, rgba(0,0,0,.2), rgba(0,0,0,0))) top/100% var(--top),
+        var(--scroll-shadow-bottom, radial-gradient(farthest-side at 50% 100%, rgba(0,0,0,.2), rgba(0,0,0,0))) bottom/100% var(--bottom),
+        var(--scroll-shadow-left, radial-gradient(farthest-side at 0%, rgba(0,0,0,.2), rgba(0,0,0,0))) left/var(--left) 100%,
+        var(--scroll-shadow-right, radial-gradient(farthest-side at 100%, rgba(0,0,0,.2), rgba(0,0,0,0))) right/var(--right) 100%;
+      background-repeat: no-repeat;
+    }
+  </style>
+  <slot></slot>
+  <s></s>
+`
 
 const updaters = new WeakMap()
 
@@ -19,37 +47,7 @@ export class ScrollShadowElement extends HTMLElement {
 
   constructor() {
     super()
-
-    this.attachShadow({ mode: 'open' }).innerHTML = `
-      <style>
-        :host {
-          display: inline-block;
-          position: relative;
-        }
-
-        :host([hidden]) {
-          display: none;
-        }
-
-        s {
-          position: absolute;
-          top: 0;
-          bottom: 0;
-          left: 0;
-          right: 0;
-          pointer-events: none;
-          background:
-            var(--scroll-shadow-top, radial-gradient(farthest-side at 50% 0%, rgba(0,0,0,.2), rgba(0,0,0,0))) top/100% var(--top),
-            var(--scroll-shadow-bottom, radial-gradient(farthest-side at 50% 100%, rgba(0,0,0,.2), rgba(0,0,0,0))) bottom/100% var(--bottom),
-            var(--scroll-shadow-left, radial-gradient(farthest-side at 0%, rgba(0,0,0,.2), rgba(0,0,0,0))) left/var(--left) 100%,
-            var(--scroll-shadow-right, radial-gradient(farthest-side at 100%, rgba(0,0,0,.2), rgba(0,0,0,0))) right/var(--right) 100%;
-          background-repeat: no-repeat;
-        }
-      </style>
-      <slot></slot>
-      <s></s>
-    `
-
+    this.attachShadow({ mode: 'open' }).innerHTML = template
     updaters.set(this, new Updater(this.shadowRoot.lastElementChild))
   }
 
@@ -72,6 +70,87 @@ export class ScrollShadowElement extends HTMLElement {
   private start() {
     const el = this.scrollEl || this.firstElementChild
     updaters.get(this).start(el, this.scrollEl ? this.firstElementChild : null)
+  }
+}
+
+class Updater {
+  private handleScroll: () => void
+  private resizeObserver: ResizeObserver
+  private el?: HTMLElement
+  private rootEl?: HTMLElement
+
+  constructor(targetElement: Element) {
+    const update = this.update.bind(this, targetElement, getComputedStyle(targetElement))
+
+    this.handleScroll = throttle(update)
+    this.resizeObserver = new ResizeObserver(update)
+  }
+
+  start(element?: HTMLElement, rootElement?: HTMLElement) {
+    if (this.el) this.stop()
+    if (element) {
+      element.addEventListener('scroll', this.handleScroll)
+      this.resizeObserver.observe(element)
+      this.el = element
+    }
+    if (rootElement) {
+      this.resizeObserver.observe(rootElement)
+      this.rootEl = rootElement
+    }
+  }
+
+  stop() {
+    if (!this.el) return
+    this.el.removeEventListener('scroll', this.handleScroll)
+    this.resizeObserver.disconnect()
+    this.el = null
+    this.rootEl = null
+  }
+
+  update(targetElement: HTMLElement, computedStyle: CSSStyleDeclaration) {
+    const { el, rootEl } = this
+    if (!el) return
+
+    const maxSize = Number(computedStyle.getPropertyValue('--scroll-shadow-size') || 14)
+    const style = {
+      '--top': clamp(el.scrollTop, 0, maxSize),
+      '--bottom': clamp(el.scrollHeight - el.offsetHeight - el.scrollTop, 0, maxSize),
+      '--left': clamp(el.scrollLeft, 0, maxSize),
+      '--right': clamp(el.scrollWidth - el.offsetWidth - el.scrollLeft, 0, maxSize),
+    }
+
+    if (rootEl) {
+      const clientRect = el.getBoundingClientRect()
+      const rootClientRect = rootEl.getBoundingClientRect()
+
+      Object.assign(style, {
+        top: clamp(clientRect.top - rootClientRect.top),
+        bottom: clamp(rootClientRect.bottom - clientRect.bottom),
+        left: clamp(clientRect.left - rootClientRect.left),
+        right: clamp(rootClientRect.right - clientRect.right),
+      })
+    }
+
+    for (const key in style) {
+      targetElement.style.setProperty(key, `${style[key]}px`)
+    }
+  }
+}
+
+function clamp(num: number, min = 0, max?: number) {
+  if (num < min) return min
+  if (num > max) return max
+  return num
+}
+
+function throttle(callback: () => void) {
+  let id = null
+  return () => {
+    if (id) return
+    id = requestAnimationFrame(() => {
+      callback()
+      id = null
+    })
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import { ScrollShadowElement } from './ScrollShadowElement'
-import './global.d.ts'
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'scroll-shadow': ScrollShadowElement
+  }
+
+  namespace JSX {
+    interface IntrinsicElements {
+      'scroll-shadow': {
+        el?: string
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        [prop: string]: any
+      }
+    }
+  }
+}
 
 export { ScrollShadowElement }
 
