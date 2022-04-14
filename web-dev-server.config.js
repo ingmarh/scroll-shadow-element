87737c09687a462f413bb121fd7e350d105585c0
Ingmar Hergst <hergst@gmail.com>
2022-04-14 20:59:39 +0200

    Server-side replace demo script src for development

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -2,6 +2,20 @@ import { esbuildPlugin } from '@web/dev-server-esbuild'
 
 export default {
   nodeResolve: true,
-  plugins: [esbuildPlugin({ ts: true })],
-  watch: true,
+  plugins: [
+    esbuildPlugin({ ts: true }),
+    {
+      name: 'html-dev-src-script-replace-plugin',
+      transform(context) {
+        if (context.response.is('html')) {
+          return {
+            body: context.body.replace(
+              /(<script\s.*)src=".+?"\s(.*)data-dev-src="(.+?)"(.+?>)/g,
+              '$1src="$3"$2$4'
+            )
+          }
+        }
+      },
+    },
+  ],
 }
